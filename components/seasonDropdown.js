0a05056ecf2a02d903a35fedfaaced06e5683829
mike14747 <mike4747@oh.rr.com>
2020-11-30 12:16:56 -0500

    Got the current view working next to the standings page's seasons dropdown.

--- components/seasonDropdown.js
+++ components/seasonDropdown.js
@@ -1,19 +1,32 @@
 import Link from 'next/link';
+import { Fragment } from 'react';
 
 import styles from '../styles/seasonDropdown.module.css';
 import PropTypes from 'prop-types';
 
-function Dropdown({ currentSeason, buttonText, listItems, handleSeasonId }) {
-    // console.log('listItems', listItems);
+function Dropdown({ displayedSeason, buttonText, listItems }) {
     return (
         <>
+            {displayedSeason &&
+                <Fragment>
+                    <span className="small">Current View:</span> <span className="font-weight-bolder">{displayedSeason.season_name}, {displayedSeason.year}</span>
+                </Fragment>
+            }
             <div className={styles.dropdown}>
                 <button className={styles.dropbtn}>{buttonText}<div className={styles.down}></div></button>
                 <ul className={styles.dropdownContent}>
                     {listItems.map(item => (
-                        <Link href={'/standings/' + item.season_id} key={item.season_id}>
-                            <a><li>{item.season_name + ' - ' + item.year}</li></a>
-                        </Link>
+                        <Fragment key={item.season_id}>
+                            {displayedSeason && (item.season_id === parseInt(displayedSeason.season_id))
+                                ? <li className={styles.viewing}>&gt; {item.season_name + ' - ' + item.year}</li>
+                                : <>
+                                    <Link href={'/standings/' + item.season_id}>
+                                        <a><li>{item.season_name + ' - ' + item.year}</li></a>
+                                    </Link>
+                                </>
+                            }
+
+                        </Fragment>
                     ))}
                 </ul>
             </div>
@@ -22,10 +35,9 @@ function Dropdown({ currentSeason, buttonText, listItems, handleSeasonId }) {
 }
 
 Dropdown.propTypes = {
-    currentSeason: PropTypes.object,
+    displayedSeason: PropTypes.object,
     buttonText: PropTypes.string,
     listItems: PropTypes.array,
-    handleSeasonId: PropTypes.func,
 };
 
 export default Dropdown;
--- pages/standings/[id].js
+++ pages/standings/[id].js
@@ -9,7 +9,7 @@ import PageHeading from '../../components/pageHeading';
 import StandingsTables from '../../components/standingsTables/standingsTables';
 import SeasonDropdown from '../../components/seasonDropdown';
 
-const Standings = ({ standings, seasons, error }) => {
+const Standings = ({ standings, displayedSeason, seasons, error }) => {
     return (
         <>
             <Head>
@@ -19,7 +19,7 @@ const Standings = ({ standings, seasons, error }) => {
             <div className="row mb-4">
                 <div className="col-12 text-right p-2">
                     {seasons && seasons.length > 0 &&
-                        <SeasonDropdown currentSeason={{}} buttonText="View Standings From" listItems={seasons} handleSeasonId={() => {}} />
+                        <SeasonDropdown displayedSeason={displayedSeason} buttonText="View Standings From" listItems={seasons} />
                     }
                 </div>
             </div>
@@ -35,6 +35,7 @@ const Standings = ({ standings, seasons, error }) => {
 
 Standings.propTypes = {
     standings: PropTypes.array,
+    displayedSeason: PropTypes.object,
     seasons: PropTypes.array,
     error: PropTypes.object,
 };
@@ -43,12 +44,13 @@ export async function getServerSideProps({ params }) {
     try {
         const standings = await db.query(SQL`SELECT st.standings_id, CONCAT(st.store_id,st.division_id) AS store_division, s.store_id, s.store_city, t.team_id, t.team_name, st.wins, st.losses, st.ties, st.total_points, d.division_id, d.day_name FROM standings AS st JOIN stores AS s ON (st.store_id=s.store_id) JOIN teams AS t ON (st.team_id=t.team_id) JOIN divisions AS d ON (st.division_id=d.division_id) WHERE st.season_id=${params.id} ORDER BY s.store_city ASC, d.division_id ASC, st.standings_order ASC;`);
 
+        const displayedSeasonResponse = await db.query(SQL`SELECT se.season_id, se.season_name, se.year FROM seasons AS se WHERE se.season_id=${params.id} LIMIT 1;`);
+        const displayedSeason = displayedSeasonResponse.length === 1 ? displayedSeasonResponse[0] : null;
+
         const seasons = await db.query(SQL`SELECT DISTINCT(st.season_id), se.season_id, se.season_name, se.year FROM standings AS st JOIN seasons AS se ON (st.season_id=se.season_id) ORDER BY se.season_id DESC;`);
 
-        if (!standings.error && !seasons.error) return { props: { standings: groupStandings(standings), seasons: JSON.parse(JSON.stringify(seasons)) } };
+        if (!standings.error && !displayedSeasonResponse.error && !seasons.error) return { props: { standings: groupStandings(standings), displayedSeason: JSON.parse(JSON.stringify(displayedSeason)), seasons: JSON.parse(JSON.stringify(seasons)) } };
         return { props: { error: { message: 'An error occurred trying to fetch data!' } } };
-
-        // return standings.error ? { props: { error: { message: 'An error occurred trying to fetch data!' } } } : { props: { standings: groupStandings(standings) } };
     } catch (error) {
         console.log(error.message);
         return { props: { error: { message: 'An error occurred trying to fetch data!' } } };
